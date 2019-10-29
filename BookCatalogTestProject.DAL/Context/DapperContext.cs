e48cd7767cf0b6c9016a7a297fde76ab5db55ba8
Andrey Grinko <grin9226@gmail.com>
2019-10-29 09:43:48 +0200

    Added Author entity Added Dapper to project Added empty Dapper context

---
+++ BookCatalogTestProject.DAL/Context/DapperContext.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BookCatalogTestProject.DAL.Context
+{
+    public class DapperContext
+    {
+
+    }
+}
---
+++ BookCatalogTestProject.DAL/Entities/AuthorEM.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BookCatalogTestProject.DAL.Entities
+{
+    public class AuthorEM
+    {
+        public string Name { get; set; }
+        public string Surname { get; set; }
+    }
+}
