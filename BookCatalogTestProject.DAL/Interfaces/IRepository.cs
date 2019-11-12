a0b1ddaa51bb2f719abfe12aed5cb2e34af86425
Andrey Grinko <grin9226@gmail.com>
2019-11-12 10:41:10 +0200

    added base repository

---
+++ BookCatalogTestProject.DAL/Interfaces/IRepository.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BookCatalogTestProject.DAL.Interfaces
+{
+    public interface IRepository<TEntity, Key> : IDisposable
+        where TEntity: class
+    {
+        string DbConnection { get; }
+
+        IEnumerable<TEntity> GetAll();
+
+        TEntity Get(Key id);
+
+        Key Create(TEntity entity);
+
+        void Update(TEntity entity);
+
+        void Delete(Key id);
+    }
+}
---
+++ BookCatalogTestProject.DAL/Repositories/RepostioryBase.cs
@@ -0,0 +1,51 @@
+﻿using BookCatalogTestProject.DAL.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BookCatalogTestProject.DAL.Repositories
+{
+    public abstract class RepostioryBase<TEntity, TKey> : IRepository<TEntity, TKey>
+        where TEntity : class
+    {
+        public string DbConnection
+        {
+            get;
+            private set;
+        }
+
+        protected RepostioryBase(string dbConnection)
+        {
+            this.DbConnection = dbConnection;
+        }
+
+        public IEnumerable<TEntity> GetAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public TEntity Get(TKey id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public TKey Create(TEntity entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Update(TEntity entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Delete(TKey id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public abstract void Dispose();
+    }
+}
